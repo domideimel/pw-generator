b40dce4c28c7ec5155dd8dec9540536a6340f7a8
dominikdeimel <info@dominikdeimel.com>
2023-10-15 16:49:34 +0200

    chore(service worker): removed service worker

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@ import { darkTheme, GlobalThemeOverrides, NConfigProvider, NGlobalStyle, NLayout
 import { BuiltInGlobalTheme } from 'naive-ui/es/themes/interface'
 import 'vfonts/Lato.css'
 import PasswordGenerator from './components/PasswordGenerator.vue'
-import ReloadPrompt from './components/ReloadPrompt.vue'
 
 const osThemeRef = useOsTheme()
 const theme = computed<BuiltInGlobalTheme | null>(() => (osThemeRef.value === 'dark' ? darkTheme : null))
@@ -33,7 +32,6 @@ const themeOverrides: GlobalThemeOverrides = {
         <password-generator/>
       </n-layout>
     </n-notification-provider>
-    <reload-prompt/>
   </n-config-provider>
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,6 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue(), VitePWA({
-    registerType: 'autoUpdate',
-    includeAssets: ['favicon.ico', 'robots.txt', 'pwa-192x192.png', 'pwa-512x512.png'],
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,woff2}'],
-    },
-    manifest: {
-      'name': 'PWGen',
-      'short_name': 'PWGen',
-      'theme_color': '#101014',
-      'background_color': '#101014',
-      'display': 'standalone',
-      'orientation': 'portrait',
-      'scope': '/',
-      'start_url': '/',
-      'icons': [{
-        'src': 'pwa-192x192.png',
-        'type': 'image/png',
-        'sizes': '192x192'
-      }, {
-        'src': 'pwa-512x512.png',
-        'type': 'image/png',
-        'sizes': '512x512'
-      }]
-    }
-  })]
-});
+  plugins: [vue()]
+})
